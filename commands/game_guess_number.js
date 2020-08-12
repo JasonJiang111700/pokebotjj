110457f9053430ada50539f90cd64d7626c58971
jasonjiangappdev <jiangj@bxscience.edu>
2020-08-12 13:32:24 -0400

    guess number 1-10

--- commands/game_guess_number.js
+++ commands/game_guess_number.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'game_guess_number',
 	description: 'guess the number',
 	async execute(message) {
-        var random_number = Math.floor(Math.random() * 25) + 1 //0-24 + 1
+        var random_number = Math.floor(Math.random() * 10) + 1 //0-9 + 1
         await message.channel.send("Guess a number between 1 and 25");
         return random_number;
 	},
